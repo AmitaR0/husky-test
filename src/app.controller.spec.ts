c23c904f9d7b604c7079e7fe26b24a9d5c0be28d
AmitaR0 <amita.r@beryl8.com>
2023-08-03 16:51:40 +0700

    edit properties

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -22,13 +22,5 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
-
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
   });
 });
